845c524a32bde2b01e3f06a3e5c5a93c6d30fa41
Leonardo Poggiani <37518886+Poggio98@users.noreply.github.com>
2018-12-05 21:43:03 +0100

    Update Operazioni.sql

--- Operazioni.sql
+++ Operazioni.sql
@@ -1,3 +1,5 @@
+ /*OPERAZIONI*/
+
 DELIMITER $$
  -- REGISTRAZIONE DI UN UTENTE
 DROP PROCEDURE IF EXISTS RegistraUtente $$
@@ -6,7 +8,7 @@ CREATE PROCEDURE RegistraUtente(
                                     IN _indirizzo VARCHAR(100), 
                                     IN _cognome VARCHAR(20), 
                                     IN _nome VARCHAR(20), 
-                                    IN _codficale VARCHAR(16),
+                                    IN _codfiscale VARCHAR(16),
                                     IN _numdocumento VARCHAR(9),
                                     IN _tipologia VARCHAR(50),
                                     IN _scadenza DATE,
@@ -21,25 +23,25 @@ BEGIN
     SET esiste =	(
 						SELECT 	COUNT(*)
 						FROM 	Utente U
-						WHERE	U.CodFiscale = _codficale
+						WHERE	U.CodFiscale = _codfiscale
 					);
     
     IF esiste = 1 THEN
 		SIGNAL SQLSTATE '45000'
 		SET MESSAGE_TEXT = 'Errore. Utente già esistente';
 	ELSEIF esiste = 0 THEN
-		INSERT INTO Utente(Password, Ruolo, Indirizzo, Cognome, Nome, CodFiscale, MediaVoto) VALUES (_password, 'fruitore', _indirizzo, _cognome, _nome, _codficale, 0);
+		INSERT INTO Utente(Password, Ruolo, Indirizzo, Cognome, Nome, CodFiscale, MediaVoto) VALUES (_password, 'fruitore', _indirizzo, _cognome, _nome, _codfiscale, 0);
          
 		SELECT	Id INTO idutente
         FROM 	Utente U
-        WHERE	U.CodFiscale = _codficale;
+        WHERE	U.CodFiscale = _codfiscale;
          
 		INSERT INTO Documento(NumDocumento, Id, Tipologia, Scadenza, EnteRilascio) VALUES (_numdocumento, idutente, _tipologia, _scadenza, _enterilascio);
     END IF;
  END $$
- 
- -- ELIMINAZIONE DI UN UTENTE
-DROP PROCEDURE IF EXISTS EliminazioneUtente $$
+
+
+-- ELIMINAZIONE DI UN UTENTE
 CREATE PROCEDURE EliminazioneUtente(IN _id INT)
 BEGIN
 	
@@ -58,12 +60,57 @@ BEGIN
 		SET MESSAGE_TEXT = 'Errore. Utente non esistente';
 	ELSEIF esiste = 1 THEN
 		DELETE FROM Utente WHERE Id = _id;
-		DELETE FROM ValutazioneUtente WHERE Id = _id;
-        	DELETE FROM Documento WHERE Id = _id;
+		DELETE FROM Documento WHERE Id = _id;
 		DELETE FROM Auto WHERE Id = _id;
 	END IF;
 END $$
 
+
+-- REGISTRAZIONE NUOVA AUTO
+
+DROP PROCEDURE IF EXISTS RegistrazioneAuto $$
+CREATE PROCEDURE RegistrazioneAuto	(
+										IN _targa VARCHAR(9),
+										IN _id INT, 
+										IN _NumPosti INT,
+										IN _VelocitaMax DOUBLE,
+										IN _AnnoImmatricolazione INT,
+										IN _Alimentazione VARCHAR(29),
+                                        IN _Cilindrata INT,
+                                        IN _Modello VARCHAR(29), 
+										IN _CasaProduttrice VARCHAR(29), 
+										IN _KmPercorsi  DOUBLE,
+                                        IN _QuantitaCarburante DOUBLE,
+                                        IN _CostoUsura DOUBLE,
+                                        IN _Peso DOUBLE,
+                                        IN _Connettività BOOL,
+                                        IN _Tavolino BOOL,
+                                        IN _TettoInVetro BOOL,
+                                        IN _Bagagliaio DOUBLE,
+                                        IN _ValutazioneAuto DOUBLE,
+                                        IN _RumoreMedio DOUBLE
+									)
+BEGIN
+	
+    DECLARE esiste INT DEFAULT 0;
+    
+    -- Verifico se l'utente già esiste
+    SET esiste =	(
+						SELECT 	COUNT(*)
+						FROM 	Auto A
+						WHERE	A.Targa = _targa
+					);
+    
+    IF esiste = 1 THEN
+		SIGNAL SQLSTATE '45000'
+		SET MESSAGE_TEXT = 'Errore. Auto già esistente';
+	ELSEIF esiste = 0 THEN
+		INSERT INTO Auto(Targa, Id) VALUES (_targa, _id);
+        INSERT INTO Caratteristiche(Targa, NumPosti, VelocitaMax, AnnoImmatricolazione, Alimentazione, Cilindrata, Modello, CasaProduttrice) VALUES (_targa, _NumPosti, _VelocitaMax, _AnnoImmatricolazione, _Alimentazione, _Cilindrata, _Modello, _CasaProduttrice);
+        INSERT INTO StatoIniziale(Targa, KmPercorsi, QuantitaCarburante, CostoUsura) VALUES (_targa, _KmPercorsi, _QuantitaCarburante, _CostoUsura);
+        INSERT INTO Optional(Targa, Peso, Connettivita, Tavolino, TettoInVetro, Bagagliaio, ValutazioneAuto, RumoreMedio) VALUES (_targa, _Peso, _Connettivita, _Tavolino, _TettoInVetro, _Bagagliaio, _ValutazioneAuto, _RumoreMedio);
+	END IF;
+END $$
 
 -- VISUALIZZAZIONE CARATTERISTICHE AUTO
 
@@ -72,7 +119,6 @@ CREATE PROCEDURE CaratteristicheAuto(IN _targa INT)
 BEGIN
 	
     DECLARE esiste INT DEFAULT 0;
-    DECLARE idutente INT DEFAULT 0;
     
     -- Verifico se l'utente già esiste
     SET esiste =(
@@ -85,13 +131,11 @@ BEGIN
 		SIGNAL SQLSTATE '45000'
 		SET MESSAGE_TEXT = 'Errore. Auto non esistente';
 	ELSEIF esiste = 1 THEN
-		SELECT	C.*, CM.Urbano AS ConsumoUrbano, CM.ExtraUrbano AS ConsumoExtraUrbano, CM.Misto AS ConsumoMisto
-		FROM	ConsumoMedio CM NATURAL JOIN Caratteristiche C;
+		SELECT	C.*, O.Peso, O.Connettività, O.Tavolino, O.TettoInVetro, O.Bagagliaio, O.ValutazioneAuto, O.RumoreMedio, CM.Urbano AS ConsumoUrbano, CM.ExtraUrbano AS ConsumoExtraUrbano, CM.Misto AS ConsumoMisto
+		FROM	(ConsumoMedio CM NATURAL JOIN Caratteristiche C) NATURAL JOIN Optional O;
 	END IF;
 END $$
 
 
 
-DELIMITER ;
- 
 DELIMITER ;
