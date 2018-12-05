0798351f8e3c79a25a44f38c90baa6e2e89909d9
Leonardo Poggiani <37518886+Poggio98@users.noreply.github.com>
2018-12-05 18:59:37 +0100

    Update Operazioni.sql

--- Operazioni.sql
+++ Operazioni.sql
@@ -37,4 +37,61 @@ BEGIN
 		INSERT INTO Documento(NumDocumento, Id, Tipologia, Scadenza, EnteRilascio) VALUES (_numdocumento, idutente, _tipologia, _scadenza, _enterilascio);
     END IF;
  END $$
+ 
+ -- ELIMINAZIONE DI UN UTENTE
+DROP PROCEDURE IF EXISTS EliminazioneUtente $$
+CREATE PROCEDURE EliminazioneUtente(IN _id INT)
+BEGIN
+	
+    DECLARE esiste INT DEFAULT 0;
+    DECLARE idutente INT DEFAULT 0;
+    
+    -- Verifico se l'utente già esiste
+    SET esiste =	(
+	    			SELECT 	COUNT(*)
+				FROM 	Utente U
+				WHERE	U.Id = _id
+			);
+    
+    IF esiste = 0 THEN
+		SIGNAL SQLSTATE '45000'
+		SET MESSAGE_TEXT = 'Errore. Utente non esistente';
+	ELSEIF esiste = 1 THEN
+		DELETE FROM Utente WHERE Id = _id;
+		DELETE FROM ValutazioneUtente WHERE Id = _id;
+        	DELETE FROM Documento WHERE Id = _id;
+		DELETE FROM Auto WHERE Id = _id;
+	END IF;
+END $$
+
+
+-- VISUALIZZAZIONE CARATTERISTICHE AUTO
+
+DROP PROCEDURE IF EXISTS CaratteristicheAuto $$
+CREATE PROCEDURE CaratteristicheAuto(IN _targa INT)
+BEGIN
+	
+    DECLARE esiste INT DEFAULT 0;
+    DECLARE idutente INT DEFAULT 0;
+    
+    -- Verifico se l'utente già esiste
+    SET esiste =(
+			SELECT 	COUNT(*)
+			FROM 	Auto A
+			WHERE	A.Targa = _targa
+		);
+    
+    IF esiste = 0 THEN
+		SIGNAL SQLSTATE '45000'
+		SET MESSAGE_TEXT = 'Errore. Auto non esistente';
+	ELSEIF esiste = 1 THEN
+		SELECT	C.*, CM.Urbano AS ConsumoUrbano, CM.ExtraUrbano AS ConsumoExtraUrbano, CM.Misto AS ConsumoMisto
+		FROM	ConsumoMedio CM NATURAL JOIN Caratteristiche C;
+	END IF;
+END $$
+
+
+
+DELIMITER ;
+ 
 DELIMITER ;
